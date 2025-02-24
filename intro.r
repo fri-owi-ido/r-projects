0dce61c3053b17fb583c5023a9631e222950eb81
fri-owi-ido <francisowoichoidoko@gmail.com>
2025-02-24 16:42:34 +0100

    First r commit

---
+++ intro.r
@@ -0,0 +1,19 @@
+# comment
+#print("Hello word")
+#print(5+5)
+#print(plot(1:10))
+#for (x in 1:10) {
+#    print(x)
+#}
+#name <- "John"
+#age <- 34
+#print(name)
+#print(age)
+
+#print(paste("I am", sep = " ", age))
+
+var1 <- "I am"
+var2 <- 34
+var3 <- "years old"
+print(paste(var1, var2, var3))
+
